7390992f79a9a00a4e7187eb88b018673c63cf26
sambaliga06 <118614578+sambaliga06@users.noreply.github.com>
2024-04-09 19:22:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const { Server } = require("socket.io");
+const { MongoClient } = require("mongodb");
+
+const mongoClient = new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to MongoDB
+mongoClient.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+    return;
+  }
+  console.log("Connected to MongoDB");
+
+  const db = mongoClient.db("authenticate");
+
+const io = new Server(8000, {
+  cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketidToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`Socket Connected`, socket.id);
+  socket.on("room:join", (data) => {
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    db.collection("users").insertOne({ email, socketId: socket.id, room });
+    socketidToEmailMap.set(socket.id, email);
+    io.to(room).emit("user:joined", { email, id: socket.id });
+    socket.join(room);
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    console.log("peer:nego:needed", offer);
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    console.log("peer:nego:done", ans);
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+});
+});
